3f3e21a590211b5d9bb983040dde4e1b3533b985
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-01-19 13:12:21 +0300

    Add use effect

--- app/javascript/message.js
+++ app/javascript/message.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData } from './redux/message';
 
 const Message = () => {
+    const dispatch = useDispatch();
+    const message = useSelector((store) => store.message);
+    console.log(message);
+
+    useEffect(() => {
+        dispatch(fetchData());
+    }, [dispatch]);
+
     return (
         <div className="">
             <h1 className="title-text">Hello</h1>
+            <p className="title-text">{message.message}</p>
             <Link to="/" className="buttons">Back</Link>
         </div>
     )

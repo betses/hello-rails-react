8ddf173f1e965e2621025ad54589c6cccd4c5a80
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-01-19 10:55:50 +0300

    Add tailwind created database and configured react

---
+++ app/javascript/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { Routes, Route, BrowserRouter } from 'react-router-dom';
+// import Home from './components/home';
+// import Message from './components/message';
+
+const App = () => (
+    <div className="w-full bg-[#242424] ">
+        {/* <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/message" element={<Message />} />
+            </Routes>
+        </BrowserRouter> */}
+        <h1 className="text-5xl text-indigo-600">Hello World t these!</h1>
+    </div>
+);
+
+export default App;
